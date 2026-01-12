f86602157fb3bc3d6b6dffb5b24e2d50ad15c0f6
Uwe <uhw@gmx.org>
2026-01-12 21:37:35 +0100

    fix: update image source path and version number to 0.0.4

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-32 px-16 bg-white dark:bg-black sm:items-start gap-6">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/MyNextEMailApp/public/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -18,7 +18,7 @@ export default function Home() {
         </h4>
       </main>
       <div className="fixed bottom-4 right-4 text-xs text-zinc-400">
-        Version 0.0.3
+        Version 0.0.4
       </div>
     </div>
   );
