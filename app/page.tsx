5aa7d2bbbee47e1b4547dda4c9393691eea4108a
Uwe <uhw@gmx.org>
2026-01-13 11:38:36 +0100

    feat: update APP_VERSION to 0.0.15, change image source in Home component, and add email icon SVG

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,16 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-32 px-16 bg-white dark:bg-black sm:items-start gap-6">
         <Image
           className="dark:invert"
-          src={`${basePath}/next.svg`}
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src={`${basePath}/email-opened-svgrepo-com.svg`}
+          alt="Email icon"
+          width={110}
+          height={110}
           priority
         />
         <h4>
           <span className="font-bold">MyNextEMailApp</span> is a small
-          application demonstrating how to send emails in different ways
+          application demonstrating how to send emails in different ways from a
+          Next.js Application.
         </h4>
       </main>
       <div className="fixed bottom-4 right-4 text-xs text-zinc-400">
